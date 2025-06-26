c2a90a8d8be313e633ec21bcf44efdd7d079b60b
Kai Ogden <kai.ogden03@gmail.com>
2025-06-26 11:48:04 -0400

    Refactor group class definitions to improve type handling and structure

--- Groups1stIso/Groups/Defs.lean
+++ Groups1stIso/Groups/Defs.lean
@@ -2,9 +2,7 @@ import Mathlib.Tactic
 
 namespace MyGroup
 
-variable {G : Type _}
-
-class MyGroup extends Mul G, One G, Inv G where
+class MyGroup (G: Type _) extends Mul G, One G, Inv G where
   mul_assoc : ∀ a b c : G, (a * b) * c = a * (b * c)
   one_mul : ∀ a : G, 1 * a = a
   mul_one : ∀ a : G, a * 1 = a
@@ -12,15 +10,15 @@ class MyGroup extends Mul G, One G, Inv G where
   mul_right_inv : ∀ a : G, a * a⁻¹ = 1
 
 
-class AbeGroup extends MyGroup G where
+class AbeGroup (G : Type _) extends MyGroup G where
   mul_comm : ∀ a b : G, a * b = b * a
 
-structure MySubgroup [MyGroup G] (H: set G) where
+structure MySubgroup (G : Type _) [MyGroup G] (H : Set G) where
   identity : 1 ∈ H
-  mul_closed : ∀ a b : H, a * b ∈ G
-  inv_closed : ∀ a : H, a⁻¹ ∈ G
+  mul_closed : ∀ a b : G, a ∈ H ∧ b ∈ H → a * b ∈ H
+  inv_closed : ∀ a ∈ H, a⁻¹ ∈ H
 
-structure NormalSubgroup [MyGroup G] (H: set G) extends Subgroup H where
-  normal : ∀ a : G, ∀ H : G, a * b * a⁻¹ ∈ G
+structure NormalSubgroup (G : Type _) [MyGroup G] (H : Set G) extends MySubgroup G H where
+  normal : ∀ a b : G, b ∈ H → a * b * a⁻¹ ∈ H
 
 end MyGroup
