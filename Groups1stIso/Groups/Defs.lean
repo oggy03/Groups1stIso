c750c2e57ef0494afcc1da52565b950948469edf
Hongyu Wang <hongyu.wang22@imperial.ac.uk>
2025-06-27 01:03:23 -0400

    FInish all the statement, prove quotient group is actually a group (but 4 sorry's to fix for two lemmas)

--- Groups1stIso/Groups/Defs.lean
+++ Groups1stIso/Groups/Defs.lean
@@ -1,9 +1,8 @@
 import Mathlib.Tactic
-
 namespace OurGroup
 
 class MyGroup (G: Type _) extends Mul G, One G, Inv G where
-  mul_assoc : ∀ a b c : G, (a * b) * c = a * (b * c)
+  mul_assoc : ∀ a b c : G, a * b * c = a * (b * c)
   one_mul : ∀ a : G, 1 * a = a
   mul_one : ∀ a : G, a * 1 = a
   mul_left_inv : ∀ a : G, a⁻¹ * a = 1
@@ -20,44 +19,263 @@ structure MySubgroup (G : Type _) [MyGroup G] (H : Set G) where
 structure NormalSubgroup (G : Type _) [MyGroup G] (H : Set G) extends MySubgroup G H where
   normal : ∀ (g : G) (h : H) , g * h * g⁻¹ ∈ H
 
--- structure LeftCoset (G : Type _) [MyGroup G] (H : Set G) (_: MySubgroup G H) where
---   Rep : G
---   elements : Set G := {h' | ∃ h ∈ H, h' = Rep * h}
+@[simp]
+def LeftCoset  (G : Type _) [MyGroup G] (Rep : G) (H : Set G) (_: MySubgroup G H) :
+  Set G := { g' | ∃ h ∈ H, g' = Rep * h }
 
-def LeftCoset  (G : Type _) [MyGroup G] (rep : G) (H : Set G) (_: MySubgroup G H) :
-  Set G := { g' | ∃ h ∈ H, g' = rep * h }
-
+@[simp]
 def RightCoset (G : Type _) [MyGroup G] (H : Set G) (_: MySubgroup G H) (Rep : G) : Set G :=
   {h' | ∃ h ∈ H, h' = h * Rep}
 
--- def RightCoset (G : Type _) [MyGroup G] (H : Set G) (_: MySubgroup G H) (Rep : G) : Set G :=
---   {h * Rep | h ∈ H}
-
+@[simp]
 def Trivial (G : Type _) [MyGroup G] : Set G := {1}
 
 end OurGroup
+namespace OurGroupProp
+
+open OurGroup
+variable (G : Type _) [MyGroup G]
+
+@[simp]
+lemma mul_left_cancel (a b c : G) (h : a * b = a * c) : b = c := by
+  rw [← MyGroup.one_mul b]
+  rw [← MyGroup.mul_left_inv a]
+  rw [MyGroup.mul_assoc]
+  rw [h]
+  rw [← MyGroup.mul_assoc]
+  rw [MyGroup.mul_left_inv]
+  rw [MyGroup.one_mul]
+
+@[simp]
+lemma mul_eq_of_eq_inv_mul {a b c : G} (h : b = a⁻¹ * c) : a * b = c := by
+  rw [h]
+  rw [← MyGroup.mul_assoc]
+  rw [MyGroup.mul_right_inv]
+  rw [MyGroup.one_mul]
+
+@[simp]
+lemma eq_mul_inv_of_mul_eq {a b c: G} (h : a * b = c) : a = c * b⁻¹ := by
+  rw [← h]
+  rw [MyGroup.mul_assoc]
+  rw [MyGroup.mul_right_inv]
+  rw [MyGroup.mul_one]
+
+@[simp]
+lemma eq_inv_mul_of_mul_eq {a b c : G} (h : b * a = c) : a = b⁻¹ * c := by
+  rw [← h]
+  rw [← MyGroup.mul_assoc]
+  rw [MyGroup.mul_left_inv]
+  rw [MyGroup.one_mul]
+
+@[simp]
+lemma mul_left_eq_self (a b : G) : a * b = b ↔ a = 1 := by
+  constructor
+  · intro h
+    have h1 := eq_mul_inv_of_mul_eq (G := G) h
+    rw [MyGroup.mul_right_inv] at h1
+    exact h1
+  · intro h
+    rw [h]
+    rw [MyGroup.one_mul]
+
+@[simp]
+lemma eq_inv_of_mul_eq_one {a b : G} (h : a * b = 1) : a = b⁻¹ := by
+  rw [← MyGroup.one_mul b⁻¹]
+  apply eq_mul_inv_of_mul_eq (G := G) h
+
+@[simp]
+lemma inv_inv (a : G) : a ⁻¹ ⁻¹ = a := by
+  symm
+  apply eq_inv_of_mul_eq_one
+  exact MyGroup.mul_right_inv a
+
+@[simp]
+lemma inv_eq_of_mul_eq_one {a b : G} (h : a * b = 1) : a⁻¹ = b := by
+  replace h := eq_mul_inv_of_mul_eq (G := G) h
+  rw [MyGroup.one_mul] at h
+  rw [h]
+  rw [inv_inv]
+
+@[simp]
+lemma unique_id {e : G} (h : ∀ x : G, e * x = x) : e = 1 := by
+  have h1 : e = e * 1 := by rw [MyGroup.mul_one]
+  rw [h1]
+  apply h 1
+
+@[simp]
+lemma unique_inv {a b : G} (h : a * b = 1) : b = a⁻¹ := by
+  apply mul_left_cancel
+  rw [h, MyGroup.mul_right_inv a]
+
+lemma mul_right_cancel {a b c : G} (h : b * a = c * a) : b = c := by
+  calc b = b * 1 := by rw [MyGroup.mul_one]
+       _ = b * (a * a⁻¹) := by rw [MyGroup.mul_right_inv]
+       _ = b * a * a⁻¹ := by rw [MyGroup.mul_assoc]
+       _ = c * a * a⁻¹  := by rw [h]
+       _ = c * (a * a⁻¹) := by rw [MyGroup.mul_assoc]
+       _ = c * 1 := by rw [MyGroup.mul_right_inv]
+       _ = c := by rw [MyGroup.mul_one]
+
+@[simp]
+lemma mul_left_cancel_iff (a x y : G) : a * x = a * y ↔ x = y := by
+  constructor
+  · intro h
+    apply mul_left_cancel
+    exact h
+  · intro h
+    rw [h]
+
+@[simp]
+lemma mul_right_cancel_iff (a x y : G) : x * a = y * a ↔ x = y := by
+  constructor
+  · intro h
+    apply mul_right_cancel
+    exact h
+  · intro h
+    rw [h]
+
+@[simp]
+lemma inv_mul (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by
+  apply mul_left_cancel
+  rw [MyGroup.mul_right_inv]
+  rw [MyGroup.mul_assoc]
+  rw [← MyGroup.mul_assoc b]
+  rw [MyGroup.mul_right_inv]
+  rw [MyGroup.one_mul]
+  rw [MyGroup.mul_right_inv]
+
+lemma one_inv : (1 : G)⁻¹ = 1 := by
+  apply mul_left_cancel
+  rw [MyGroup.mul_right_inv]
+  rw [MyGroup.one_mul]
+
+@[simp]
+theorem inv_eq (a b : G): a⁻¹ = b ↔ b⁻¹ = a := by
+  constructor
+  · rintro rfl
+    rw [inv_inv]
+  · intro h
+    rw [← h]
+    rw [inv_inv]
+
+end OurGroupProp
 
 namespace OurQuotient
-open OurGroup
+open OurGroup OurGroupProp
 variable {G : Type _} [MyGroup G]
 variable {H : Set G} (n : NormalSubgroup G H)
 
-def QuotientSet : Set (Set G) := { s | ∃ g : G, s = LeftCoset G g H n.toMySubgroup }
+@[simp]
+def NormEquiv (_ : NormalSubgroup G H) (a b : G) : Prop := a * b⁻¹ ∈ H
 
-abbrev Coset := {s : Set G // s ∈ QuotientSet n}
+instance NormEquiv_is_equiv : Equivalence (NormEquiv n) where
+  refl := by
+        intro x
+        simp only [NormEquiv]
+        have h1 : x * x⁻¹ = 1 := MyGroup.mul_right_inv x
+        have h2 : 1 ∈ H := n.identity
+        exact Set.mem_of_eq_of_mem h1 h2
+  symm := by
+      intro x y h
+      simp only [NormEquiv] at *
+      have h_inv : (x * y⁻¹)⁻¹ ∈ H := n.inv_closed _ h
+      rw [inv_mul, inv_inv] at h_inv
+      exact h_inv
+  trans := by
+      intro x y z hxy hyz
+      simp only [NormEquiv] at *
+      have h_mul : (x * y⁻¹) * (y * z⁻¹) ∈ H := n.mul_closed _ _ ⟨hxy, hyz⟩
+      have h_calc : (x * y⁻¹) * (y * z⁻¹) = x * z⁻¹ := by
+        calc (x * y⁻¹) * (y * z⁻¹)
+          _ = x * (y⁻¹ * (y * z⁻¹)) := by rw [MyGroup.mul_assoc]
+          _ = x * ((y⁻¹ * y) * z⁻¹) := by rw [← MyGroup.mul_assoc y⁻¹]
+          _ = x * (1 * z⁻¹)         := by rw [MyGroup.mul_left_inv]
+          _ = x * z⁻¹               := by rw [MyGroup.one_mul]
+      rw [h_calc] at h_mul
+      exact h_mul
 
-noncomputable def QuotientMul : Coset n → Coset n → Coset n :=
-  fun A B =>
-    -- A and B are pairs: A.val is the set, A.property is the proof it's a coset.
-    -- From A.property, we know `∃ g, A.val = LeftCoset...`. `choose` picks one such `g`.
-    let gA := Classical.choose A.property
-    let gB := Classical.choose B.property
-    -- The resulting set is the coset of the product of the representatives.
-    let C_val := LeftCoset G (gA * gB) H n.toMySubgroup
-    -- We must prove this resulting set is also a valid coset.
-    have C_property : C_val ∈ QuotientSet n := by use (gA * gB)
-    -- The proof is that `gA * gB` is a valid representative.
-    -- Return the new coset, which is the pair of the set and the proof.
-    ⟨C_val, C_property⟩
+def MyGroupSetoid : Setoid G where
+  r := NormEquiv n
+  iseqv := NormEquiv_is_equiv n
+
+def MyQuotientGroup : Type _ := Quotient (MyGroupSetoid n)
+
+lemma NormEquiv_mul
+    {a₁ a₂ b₁ b₂ : G}
+    (ha : NormEquiv n a₁ a₂) (hb : NormEquiv n b₁ b₂)
+    : NormEquiv n (a₁ * b₁) (a₂ * b₂) := by
+  dsimp [NormEquiv] at *
+  have : (a₁ * b₁) * (a₂ * b₂)⁻¹ =
+         (a₁ * a₂⁻¹) * (b₁ * b₂⁻¹) := by sorry
+  have h₁ : a₁ * a₂⁻¹ ∈ H := ha
+  have h₂ : b₁ * b₂⁻¹ ∈ H := hb
+  have : (a₁ * b₁) * (a₂ * b₂)⁻¹ ∈ H := by sorry
+  exact this
+
+lemma NormEquiv_inv {a b : G} (h : NormEquiv n a b)
+  : NormEquiv n a⁻¹ b⁻¹ := by
+  dsimp [NormEquiv] at *
+  refine MySubgroup.mul_closed ?_ a⁻¹ b⁻¹⁻¹ ?_
+  · exact n.toMySubgroup
+  · constructor
+    · sorry
+    · sorry
+
+def MulQuot : MyQuotientGroup n → MyQuotientGroup n → MyQuotientGroup n :=
+  Quotient.map₂ (fun a b ↦ a * b)
+    (by
+      intro a₁ a₂ ha b₁ b₂ hb
+      exact NormEquiv_mul n ha hb)
+
+def InvQuot : MyQuotientGroup n → MyQuotientGroup n :=
+  Quotient.map (fun a ↦ a⁻¹)
+    (by
+      intro a b h
+      exact NormEquiv_inv n h)
+
+instance MyQuotientGroup_is_group (n : NormalSubgroup G H) : MyGroup (MyQuotientGroup n) where
+  mul := MulQuot n
+  one := ⟦1⟧
+  inv := InvQuot n
+  mul_assoc := by
+              intro G H K
+              refine Quotient.inductionOn₃ G H K (fun g h k ↦ ?_)
+              apply Quotient.sound
+              dsimp [NormEquiv]
+              have h_eq : g * h * k = g * (h * k) := by rw [MyGroup.mul_assoc]
+              have : ((g * h) * k) * (g * (h * k))⁻¹ = 1 := by rw [h_eq, MyGroup.mul_right_inv]
+              exact Quotient.exact (congrArg (Quotient.mk (MyGroupSetoid n)) h_eq)
+  one_mul := by
+          intro a
+          refine Quotient.inductionOn a (fun g ↦ ?_)
+          apply Quotient.sound
+          dsimp [NormEquiv]
+          have h_eq : 1 * g = g := by rw [MyGroup.one_mul]
+          have : 1 * g * (1 * g)⁻¹ = 1 := by rw [h_eq, MyGroup.mul_right_inv]
+          exact Quotient.exact (congrArg (Quotient.mk (MyGroupSetoid n)) h_eq)
+  mul_one := by
+          intro a
+          refine Quotient.inductionOn a (fun g ↦ ?_)
+          apply Quotient.sound
+          dsimp [NormEquiv]
+          have h_eq : g * 1 = g := by rw [MyGroup.mul_one]
+          have : g * 1 * (g * 1)⁻¹ = 1 := by rw [h_eq, MyGroup.mul_right_inv]
+          exact Quotient.exact (congrArg (Quotient.mk (MyGroupSetoid n)) h_eq)
+  mul_left_inv := by
+          intro a
+          refine Quotient.inductionOn a (fun g ↦ ?_)
+          apply Quotient.sound
+          dsimp [NormEquiv]
+          have h_eq : g⁻¹ * g = 1 := by rw [MyGroup.mul_left_inv]
+          have : g⁻¹ * g * (g⁻¹ * g)⁻¹ = 1 := by rw [h_eq, MyGroup.mul_right_inv]
+          exact Quotient.exact (congrArg (Quotient.mk (MyGroupSetoid n)) h_eq)
+  mul_right_inv := by
+          intro a
+          refine Quotient.inductionOn a (fun g ↦ ?_)
+          apply Quotient.sound
+          dsimp [NormEquiv]
+          have h_eq : g * g⁻¹ = 1 := by rw [MyGroup.mul_right_inv]
+          have : g * g⁻¹ * (g * g⁻¹)⁻¹ = 1 := by rw [h_eq, MyGroup.mul_right_inv]
+          exact Quotient.exact (congrArg (Quotient.mk (MyGroupSetoid n)) h_eq)
 
 end OurQuotient
--- Groups1stIso/Homomorphism/Defs.lean
+++ Groups1stIso/Homomorphism/Defs.lean
@@ -23,3 +23,25 @@ def Myim [MyGroupHom f] : Set H :=
   Set.range f
 
 end OurGroupHom
+
+open OurGroupHom OurGroup OurGroupProp OurQuotient
+
+namespace OurFirstIso
+
+variable {G H : Type _} [MyGroup G] [MyGroup H]
+
+-- Kernel of a group homomorphism is a normal subgroup
+instance Myker_is_normal (f : G → H) [MyGroupHom f] :
+  NormalSubgroup G (Myker f) := by sorry
+
+-- Image of a group homomorphism is a group
+instance Myim_is_group (f : G → H) [MyGroupHom f] :
+  MyGroup (Myim f) := by sorry
+
+-- The first isomorphism theorem
+theorem FirstIsoTheorem (f : G → H) [MyGroupHom f] :
+  ∃ φ : MyQuotientGroup (Myker_is_normal f) → Myim f,
+    MyGroupHom φ ∧
+    Function.Bijective φ := by sorry
+
+end OurFirstIso
